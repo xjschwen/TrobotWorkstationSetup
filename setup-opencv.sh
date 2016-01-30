7090ba8b215f9b555fb996d24139135cedb33260
Jeff Schwennesen <xjschwen@gmail.com>
2016-01-30 12:19:53 -0500

    jas improve the setup

--- setup-opencv.sh
+++ setup-opencv.sh
@@ -4,31 +4,33 @@
 #http://www.pyimagesearch.com/2015/06/22/install-opencv-3-0-and-python-2-7-on-ubuntu/
 
 
+
+
 #Step 1:  Make sure that we are upto date with the OS
 sudo apt-get update
 sudo apt-get upgrade
 
 #Step 2: First round of developer tools
-sudo apt-get install build-essential cmake git pkg-config
+sudo apt-get -y install build-essential cmake git pkg-config
 
 
 #Step 3: Image file format utilities
-sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
 
 #Step 4: Image display utilities
-sudo apt-get install libgtk2.0-dev
+sudo apt-get -y install libgtk2.0-dev
 
 #Step 5: Image stream utilites  (Video stream readers)
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 
 
 #Step 6: Optomize it for various processors 
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get -y install libatlas-base-dev gfortran
 
 
 #Step 7: Install PIP
-wget https://bootstrap.pypa.io/get-pip.py 
-sudo python get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py  -P /tmp 
+sudo python /tmp/get-pip.py
 
 
 #Step 8 use pip to install some useful packages
@@ -47,11 +49,13 @@ source ~/.bashrc
 
 #Make the cv virtual environment
 mkvirtualenv cv
-
+workon cv
 
 #Step 9  install python 2.7 and numpy
 sudo apt-get install python2.7-dev
 pip install numpy
+pip install imutils
+
 
 #Step 10 get opencv  and opencv_contrib from github
 
@@ -92,21 +96,21 @@ fi
 #config the build scripts
 
 cd $OPENCV_ROOT
+export JAVA_HOME=/opt/java/jdk1.8
+
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D INSTALL_C_EXAMPLES=OFF \
+	-D BUILD_SHARED_LIBS=OFF \
+	-D CMAKE_INSTALL_PREFIX=/opt/opencv \
+	-D INSTALL_C_EXAMPLSES=OFF \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
 	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
 	-D BUILD_EXAMPLES=ON ..
 
 #compile using 2 cores.
-make -j2
+sudo make -j2
 
 # install the open cv
 sudo make install
 sudo ldconfig
-
-
-
