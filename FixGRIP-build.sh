ea6bafd9953ef48b87ba5b10453c3d38de44fc34
WhitmoreLakeTroBots <wlhsroboticsteam@gmail.com>
2016-03-02 11:13:44 -0500

    change to 755 perms

---
+++ FixGRIP-build.sh
@@ -0,0 +1,7 @@
+
+
+#From  http://askubuntu.com/questions/575505/glibcxx-3-4-20-not-found-how-to-fix-this-error
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
