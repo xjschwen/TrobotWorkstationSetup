9dc460d703c7c8e9af2a776628e273919c69ad4a
Jeff <xjschwen@gmail.com>
2017-06-04 20:26:11 -0400

    install google chrome

--- setup-chrome.sh
+++ setup-chrome.sh
@@ -1,7 +1,7 @@
 cd ~/Downloads
-
+rm -f ~/Downloads/google-chrome-stable_current_amd64.deb*
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 
 sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb 
-sudo apt-get install -f .
+sudo apt-get install -y -f google-chrome-stable
