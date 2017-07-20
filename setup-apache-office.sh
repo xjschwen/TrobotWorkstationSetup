354c9932d753f86a72cb73dc76e7a036e7d8e844
Mr <xjschwen@gmail.com>
2017-07-19 22:26:26 -0400

    final tweaks

--- setup-apache-office.sh
+++ setup-apache-office.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #Remove libreoffice 
-sudo apt-get -f -y remove --purge libreoffice*
-sudo apt-get -f -y clean
-sudo apt-get -f -y autoremove
+sudo apt-get -y remove --purge libreoffice*
+sudo apt-get -y clean
+sudo apt-get -y autoremove
 
-#Download from the web
-wget https://superb-sea2.dl.sourceforge.net/project/openofficeorg.mirror/4.1.3/binaries/en-US/Apache_OpenOffice_4.1.3_Linux_x86-64_install-deb_en-US.tar.gz
-
-mv Apache_OpenOffice_4.1.3_Linux_x86-64_install-deb_en-US.tar.gz ~/Downloads/
 cd ~/Downloads/
+
+# if we already have the file do not waste time downloading it again.
+#Download from the web
+if [ ! -f Apache_OpenOffice_4.1.3_Linux_x86-64_install-deb_en-US.tar.gz ] ; then
+	wget https://superb-sea2.dl.sourceforge.net/project/openofficeorg.mirror/4.1.3/binaries/en-US/Apache_OpenOffice_4.1.3_Linux_x86-64_install-deb_en-US.tar.gz
+fi
 
 #Install the apache office suite
 tar -xzvf Apache_OpenOffice_4.1.3_Linux_x86-64_install-deb_en-US.tar.gz
@@ -17,3 +19,6 @@ cd en-US/DEBS
 sudo dpkg -i *.deb
 cd desktop-integration
 sudo dpkg -i *.deb
+
+#perform some cleanup
+rm -Rf ~/Downloads/en-US/
