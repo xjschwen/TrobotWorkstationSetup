3e65aa51afa3bba1a4aafc2a6af40f4c812e2262
Jeff Schwennesen <xjschwen@gmail.com>
2017-12-22 15:43:25 -0500

    jas edits to .bashrc and path

--- linux/common/load-java1-8.sh
+++ linux/common/load-java1-8.sh
@@ -11,20 +11,20 @@ curdir=$(readlink -m $HOME)
 # if pi use pi name
 if [ "$(cat /proc/cpuinfo | grep "ARMv7")"  > "0" ] ; then
 	javaInstaller="jdk-8u151-linux-arm64-vfp-hflt.tar.gz"
-
 else
 	javaInstaller="jdk-8u151-linux-x64.tar.gz"
 fi
 
+jdk_tar=~/$javaInstaller
 
 if [ -z ${jdk_tar} ] ; then
 	jdk_tar=$(find /media/ -name ${javaInstaller} | sort | tail -1)
 fi
+
 if [ -z ${jdk_tar} ] ; then
 	echo "ERROR: Could not locate the correct installer"
 	echo "Looking for ${javaInstaller}"
 	exit 1
-
 fi 
 
 jdkName="jdk1.8.0_151"
@@ -46,4 +46,18 @@ sudo ln -fs "$jdkName" "jdk1.8"
 popd
 
 sudo cp environment /etc/environment
+
+cat ~/.bashrc |  sed 's#export\sJAVA_HOME\s*=.*#export JAVA_HOME=/opt/java/jdk1.8#g' > ~/.bashrc_new
+rm  -f ~/bashrc
+mv ~/.bashrc_new ~/.bashrc
+
+export JAVA_HOME=/opt/java/jdk1.8
+cat ~/.bashrc |  sed 's#export\sPATH\s*=.*##g' > ~/.bashrc_new
+echo "export PATH=${JAVA_HOME}:${JAVA_HOME}/bin:${PATH}" >> ~/.bashrc_new
+rm  -f ~/bashrc
+mv ~/.bashrc_new ~/.bashrc
+
+ 
+
+
 
