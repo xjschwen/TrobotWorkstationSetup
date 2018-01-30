ef095b1c21f0828b5819da8c0604335d4a9177ac
Jeff <xjschwen@gmail.com>
2018-01-29 19:10:25 -0500

    Fix typo for testing

--- linux/rpi/setup-rpi-uv4l-camera-streaming.sh
+++ linux/rpi/setup-rpi-uv4l-camera-streaming.sh
@@ -30,7 +30,7 @@ if [ ${jessie} > 0  ] ; then
  newline="http://www.linux-projects.org/listing/uv4l_repo/raspbian/ jessie main"
 elif [ ${wheezy} > 0  ] ; then
  newline="http://www.linux-projects.org/listing/uv4l_repo/raspbian/ jessie main"
-elfi [ ${stretch} ] ; then
+elif [ ${stretch} ] ; then
   newline="deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main"
 else
   echo "Unsupported PI operating system."
